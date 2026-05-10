af34f989c8640166feef14117398272fe0efda9a
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-10 23:00:40 +0800

    del: Delete libs directory

--- libs/builtins/include/builtins_lib.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../../src/models/models.hpp"
-#include "builtin_functions.hpp"
-#include "builtin_methods.hpp"
-
-namespace builtins_lib {
-model::Object* init_module(model::Object* self, const model::List* args);
-}
