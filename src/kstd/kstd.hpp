cbc3618e1e757d8310f125eef50eccf45029f8e8
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-11 08:12:13 +0800

    feat: create kstd.hpp

---
+++ src/kstd/kstd.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+//| The kiz std library (lstd)
+//| author: azhz@azhz1107cat@outlook.com
+//|
+//|
+//|     _        _      _ 
+//|    | | _____| |_ __| |
+//|    | |/ / __| __/ _` |
+//|    |   <\__ \ || (_| |
+//|    |_|\_\___/\__\__,_|
+//|              
+
+namespace kstd {
+#include "ptr.hpp"
+#include "str.hpp"
+#include "vec.hpp"
+#include "option.hpp"
+#include "result.hpp"
+
+using uint32 = uint32_t;
+using int32 = int32_t;
+using uint8 = uint8_t;
+}
