81cf044dd466d69df258b93fd4c37f38609824f6
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-15 19:26:30 +0800

    feat: add some macros in kstd.hpp

--- src/kstd/kstd.hpp
+++ src/kstd/kstd.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-//|    The kizc&kizrt's based standard library (kstd)
-//|    author: azhz@azhz1107cat@outlook.com
+//|    The kizc & kizrt's based standard library (kstd)
+//|    author: azhz<azhz1107cat@outlook.com>
 //|
 //|     _        _      _ 
 //|    | | _____| |_ __| |
@@ -16,6 +16,22 @@ namespace kstd {
 #include "vec.hpp"
 #include "option.hpp"
 #include "result.hpp"
+
+#define $VecForeach(i,v) for (auto i = 0; i < v.len(); ++1)
+#ifdef _MSC_VER
+#   define $Likely(x) (x)
+#   define $Unlikely(x) (x)
+#else
+#   define $Likely(x)   __builtin_expect(!!(x), 1)
+#   define $Unlikely(x) __builtin_expect(!!(x), 0)
+#endif
+
+#define $IsWin  defined(_WIN32) || defined(_WIN64)
+#define $IsLinux defined(__linux__)
+#define $IsMac  defined(__APPLE__)
+
+#define $IsX64  defined(__x86_64__) || defined(_M_X64)
+#define $IsArm64 defined(__aarch64__) || defined(_M_ARM64)
 
 using uint32 = uint32_t;
 using int32 = int32_t;
