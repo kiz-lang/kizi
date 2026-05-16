9ee7a775ca319cbd9894c03c604d6e1100dcae69
azhz <105213176+azhz1107cat@users.noreply.github.com>
2026-05-16 20:45:06 +0800

    fix: fix the x-macro

--- src/kiz_mir/opcode.hpp
+++ src/kiz_mir/opcode.hpp
@@ -2,13 +2,13 @@
 
 namespace kmir {
 
-#define $Op(x) x
+#define $Op(x, y) x,
 enum class Opcode: uint8_t {
 #   include "opcode_kind.def"
 };
 #undef $Op
 
-#define $Op(x) case x: return #x
+#define $Op(x, y) case x: return Str(#x);
 inline auto opcode_to_str(Opcode op) -> Str {
     switch op {
 #       include "opcode_kind.def"
